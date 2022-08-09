fd4183f7a0b3ad005fcdb386d93326412198b3a9
sh4tteredd <55893559+sh4tteredd@users.noreply.github.com>
2022-08-09 16:01:39 +0200

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+check_curl() {
+  if ! command -v curl > /dev/null; then
+      echo "curl is not installed!"
+      echo "In order to use this script please install wget from your package manager"
+      exit
+  fi
+}
+clean() {
+    echo "cleaning..."
+    rm -rf extracted*
+    rm LICENSE
+    rm -rf META-INF
+    rm payload_properties.txt
+    rm payload-dumper-go
+    rm fw.zip
+    rm payload.bin
+    rm README.md
+    rm apex_info.pb
+    rm care_map.pb
+}
+check_wget() {
+  if ! command -v curl > /dev/null; then
+      echo "wget is not installed!"
+      echo "In order to use this script please install wget from your package manager"
+      exit
+  fi
+}
+check_curl;
+check_wget;
+
+rm -rf images/ #clean from previous downloads
+rm payload-dumper-go.tar.gz #clean from previous downloads
+
+if [[ $(uname -m) == 'arm64' ]]; then #check if arch is arm64
+  arm="1"
+fi
+
+read -p "Do you need the [G]lobal firmware or the [E]uropean firmware (G/E)" ge 
+case $ge in 
+	G ) echo "Downloading the global firmware";
+        curl https://mde1.androidfilehost.com/dl/PfKhvf7Ic7YYylFx2wkH8Q/1660138643/15664248565197192084/Global_Nothing_OS_1.1.0_Update.zip --output fw.zip;;
+	E ) echo "Downloading the EU firmware";
+        curl https://mde1.androidfilehost.com/dl/yrsVRoAumRznxsu7QIBTow/1660130195/15664248565197192093/Europe_Nothing_OS_1.1.0_Update.zip --output fw.zip;;
+    * ) echo invalid response;
+		exit 1;;
+esac
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then #GNU/Linux
+    if [[ $arm -eq 1 ]]; then #download arm64 version if needed
+        wget -q https://github.com/ssut/payload-dumper-go/releases/download/1.2.2/payload-dumper-go_1.2.2_linux_arm64.tar.gz
+    else
+        wget -q https://github.com/ssut/payload-dumper-go/releases/download/1.2.2/payload-dumper-go_1.2.2_linux_amd64.tar.gz
+    fi
+elif [[ "$unamestr" == 'Darwin' ]]; then #macOS
+    if [[ $arm -eq 1 ]]; then #download arm64 version if needed
+        wget -q https://github.com/ssut/payload-dumper-go/releases/download/1.2.2/payload-dumper-go_1.2.2_darwin_arm64.tar.gz
+    else
+        wget -q https://github.com/ssut/payload-dumper-go/releases/download/1.2.2/payload-dumper-go_1.2.2_darwin_amd64.tar.gz
+    fi
+fi
+mv payload-dumper-go*.tar.gz payload-dumper-go.tar.gz
+tar -zxf payload-dumper-go.tar.gz
+rm payload-dumper-go.tar.gz
+unzip -q fw.zip
+./payload-dumper-go payload.bin
+echo " "
+mkdir images
+mv extracted*/* images/
+clean;
--- flash_all.sh
+++ flash_all.sh
@@ -1,65 +1,78 @@
-echo "nothing phone(1) fastboot flash v1.1.0EEA"
+echo "nothing phone(1) fastboot flash by @sh4tteredd"
+echo ""
+echo "In order to gather all the files that we need, consider to run my other script available on https://github.com/sh4tteredd/nothing-phone-1-tools"
+echo ""
+echo "If you already have the .img files, put all your files in a subfolder called 'images' to continue"
+echo ""
+read -p "Now connect your phone in fastboot mode to the PC via USB, then press enter to continue"
+
 
 fastboot $* getvar product 2>&1 | grep "^product: *lahaina"
 if [ $? -ne 0 ] ; then echo "This script is only for nothing phone(1)"; exit 1; fi
-SCRIPT_PATH=$(dirname $0)
 
-fastboot flash abl ${SCRIPT_PATH}/images/abl.img
+DIR="./images/"
+if [ ! -d "$DIR" ]; then
+  echo "The images subfolder does not exists, check your files first!"
+  exit 0
+fi
+fastboot -w
 
-fastboot flash aop ${SCRIPT_PATH}/images/aop.img
+fastboot flash abl ./images/abl.img
 
-fastboot flash bluetooth ${SCRIPT_PATH}/images/bluetooth.img
+fastboot flash aop ./images/aop.img
 
-fastboot flash boot ${SCRIPT_PATH}/images/boot.img
+fastboot flash bluetooth ./images/bluetooth.img
 
-fastboot flash cpucp ${SCRIPT_PATH}/images/cpucp.img
+fastboot flash boot ./images/boot.img
 
-fastboot flash devcfg ${SCRIPT_PATH}/images/devcfg.img
+fastboot flash cpucp ./images/cpucp.img
 
-fastboot flash dsp ${SCRIPT_PATH}/images/dsp.img
+fastboot flash devcfg ./images/devcfg.img
 
-fastboot flash dtbo ${SCRIPT_PATH}/images/dtbo.img
+fastboot flash dsp ./images/dsp.img
 
-fastboot flash featenabler ${SCRIPT_PATH}/images/featenabler.img
+fastboot flash dtbo ./images/dtbo.img
 
-fastboot flash hyp ${SCRIPT_PATH}/images/hyp.img
+fastboot flash featenabler ./images/featenabler.img
 
-fastboot flash imagefv ${SCRIPT_PATH}/images/imagefv.img
+fastboot flash hyp ./images/hyp.img
 
-fastboot flash keymaster ${SCRIPT_PATH}/images/keymaster.img
+fastboot flash imagefv ./images/imagefv.img
 
-fastboot flash modem ${SCRIPT_PATH}/images/modem.img
+fastboot flash keymaster ./images/keymaster.img
 
-fastboot flash multiimgoem ${SCRIPT_PATH}/images/multiimgoem.img
+fastboot flash modem ./images/modem.img
 
-fastboot flash odm ${SCRIPT_PATH}/images/odm.img
+fastboot flash multiimgoem ./images/multiimgoem.img
 
-fastboot flash product ${SCRIPT_PATH}/images/product.img
+fastboot flash odm ./images/odm.img
 
-fastboot flash qupfw ${SCRIPT_PATH}/images/qupfw.img
+fastboot flash product ./images/product.img
 
-fastboot flash shrm ${SCRIPT_PATH}/images/shrm.img
+fastboot flash qupfw ./images/qupfw.img
 
-fastboot flash system ${SCRIPT_PATH}/images/system.img
+fastboot flash shrm ./images/shrm.img
 
-fastboot flash system_ext ${SCRIPT_PATH}/images/system_ext.img
+fastboot flash system ./images/system.img
 
-fastboot flash tz ${SCRIPT_PATH}/images/tz.img
+fastboot flash system_ext ./images/system_ext.img
 
-fastboot flash uefisecapp ${SCRIPT_PATH}/images/uefisecapp.img
+fastboot flash tz ./images/tz.img
 
-fastboot flash vbmeta ${SCRIPT_PATH}/images/vbmeta.img
+fastboot flash uefisecapp ./images/uefisecapp.img
 
-fastboot flash vbmeta_system ${SCRIPT_PATH}/images/vbmeta_system.img
+fastboot flash vbmeta ./images/vbmeta.img
 
-fastboot flash vendor ${SCRIPT_PATH}/images/vendor.img
+fastboot flash vbmeta_system ./images/vbmeta_system.img
 
-fastboot flash vendor_boot ${SCRIPT_PATH}/images/vendor_boot.img
+fastboot flash vendor ./images/vendor.img
 
-fastboot flash xbl ${SCRIPT_PATH}/images/xbl.img
+fastboot flash vendor_boot ./images/vendor_boot.img
 
-fastboot flash xbl_config ${SCRIPT_PATH}/images/xbl_config.img
+fastboot flash xbl ./images/xbl.img
+
+fastboot flash xbl_config ./images/xbl_config.img
 
 fastboot $* reboot
 
-exit 0
+echo "Your phone(1) is rebooting"
