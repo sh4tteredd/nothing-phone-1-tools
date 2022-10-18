2a8a3010570290d8a46ffd4110bb57f3499173f4
sh4tteredd <55893559+sh4tteredd@users.noreply.github.com>
2022-10-18 19:43:39 +0200

    possibility to select a local fw.zip

--- download.ps1
+++ download.ps1
@@ -20,24 +20,35 @@ function clean {
 }
 #Test-CommandExists("fastboot")
 Write-Host "Nothing firmware downloader by @sh4ttered V1.1.4"
-Write-Host "WARNING: EU Version still 1.1.3 (waiting for the full link), Global Version 1.1.4"
-$msg = 'Do you need the [G]lobal firmware or the [E]uropean firmware (G/E)? '
-$response = Read-Host -Prompt $msg
-if ($response -eq 'g') {
-    Write-Host "Downloading the global firmware v1.1.4"
-    Write-Host "This may take a while depending on your internet speed"
-    wget -Uri https://android.googleapis.com/packages/ota-api/package/54b8dbd1c303be00ef156c602b756c76d8d9b6e1.zip -OutFile fw.zip #global 1.1.4
-}
-elseif ($response -eq 'e') {
-    Write-Host "Downloading the EU firmware v1.1.3"
-    Write-Host "This may take a while depending on your internet speed"
-    wget -Uri https://android.googleapis.com/packages/ota-api/package/a6f363b6709ec67910b4018526d9525ccb4075f9.zip -OutFile fw.zip #eu 1.1.3
+$msg1 = 'Have you already downloaded the firmware? (y/n)'
+
+$choice = Read-Host -Prompt $msg1
+if ($choice -eq 'y') {
+   #place your firmware in the same folder as this script and rename it fw.zip then press enter
+    Write-Host "Place your firmware in the same folder as this script and rename it fw.zip then press enter"
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
 else {
-    Write-Host "Invalid Input!"
-    Write-Host -NoNewLine 'Press any key to continue...';
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-    exit
+
+    Write-Host "WARNING: EU Version still 1.1.3 (waiting for the full link), Global Version 1.1.4"
+    $msg = 'Do you need the [G]lobal firmware or the [E]uropean firmware (G/E)? '
+    $response = Read-Host -Prompt $msg
+    if ($response -eq 'g') {
+        Write-Host "Downloading the global firmware v1.1.4"
+        Write-Host "This may take a while depending on your internet speed"
+        wget -Uri https://android.googleapis.com/packages/ota-api/package/54b8dbd1c303be00ef156c602b756c76d8d9b6e1.zip -OutFile fw.zip #global 1.1.4
+    }
+    elseif ($response -eq 'e') {
+        Write-Host "Downloading the EU firmware v1.1.3"
+        Write-Host "This may take a while depending on your internet speed"
+        wget -Uri https://android.googleapis.com/packages/ota-api/package/a6f363b6709ec67910b4018526d9525ccb4075f9.zip -OutFile fw.zip #eu 1.1.3
+    }
+    else {
+        Write-Host "Invalid Input!"
+        Write-Host -NoNewLine 'Press any key to continue...';
+        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+        exit
+    }
 }
 Add-Type -Assembly System.IO.Compression.FileSystem
 $zip = [IO.Compression.ZipFile]::OpenRead(".\fw.zip")
