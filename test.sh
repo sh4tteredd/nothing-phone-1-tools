1501ee7e10c39fab6aa3b6ea530408f28df69d53
Sary Alharthy <sary.alharthy8@gmail.com>
2023-03-03 22:26:40 +0300

    Clean up ignored files

--- test.sh
+++
@@ -1,8 +0,0 @@
-RED='\033[0;31m' # Red Color
-NC='\033[0m' # No Color
-GREEN='\033[0;32m' # Green Color
-
-echo "${GREEN}[*] Now connect your phone in fastboot mode to the PC via USB, then press enter to continue"
-read answer
-
-exit 0
