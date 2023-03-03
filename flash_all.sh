84a3eb0a886609bfd58c24ee73e7d9291e1bb3a3
Sary Alharthy <sary.alharthy8@gmail.com>
2023-03-03 21:50:03 +0300

    Check and Combine Slots

--- flash_all.sh
+++ flash_all.sh
@@ -22,7 +22,6 @@ echo "${GREEN}[*] Instead, if you already have the .img files, put all your file
 echo "${GREEN}[*] Now connect your phone in fastboot mode to the PC via USB, then press enter to continue"
 read answer
 
-
 #fastboot $* getvar product 2>&1 | grep "^product: *lahaina"
 #if [ $? -ne 0 ] ; then echo "This script is only for nothing phone(1)"; exit 1; fi
 
@@ -36,142 +35,49 @@ if [ ! -d "./images/" ]; then
   exit 0
 fi
 
-if ./platform-tools/fastboot getvar product 2>&1 | grep ' '
-
 ./platform-tools/fastboot -w
 
-./platform-tools/fastboot --set-active=a
+if ./platform-tools/fastboot getvar current-slot 2>&1 | grep -q "^current-slot: *a"
+  then
+    slot=a
+  else
+    slot=b
+fi
+
+./platform-tools/fastboot --set-active=$slot
 
 echo "${GREEN}${BOLD}Flashing A slot. Please wait... \n"
 
 ./platform-tools/fastboot reboot fastboot
-
-./platform-tools/fastboot flash abl_a ./images/abl.img
-
-./platform-tools/fastboot flash aop_a ./images/aop.img
-
+./platform-tools/fastboot flash abl_$slot ./images/abl.img
+./platform-tools/fastboot flash aop_$slot ./images/aop.img
 ./platform-tools/fastbootfastboot flash bluetooth_a ./images/bluetooth.img
-
-./platform-tools/fastboot flash boot_a ./images/boot.img
-
-./platform-tools/fastboot flash cpucp_a ./images/cpucp.img
-
-./platform-tools/fastboot flash devcfg_a ./images/devcfg.img
-
-./platform-tools/fastboot flash dsp_a ./images/dsp.img
-
-./platform-tools/fastboot flash dtbo_a ./images/dtbo.img
-
-./platform-tools/fastboot flash featenabler_a ./images/featenabler.img
-
-./platform-tools/fastboot flash hyp_a ./images/hyp.img
-
-./platform-tools/fastboot flash imagefv_a ./images/imagefv.img
-
-./platform-tools/fastboot flash keymaster_a ./images/keymaster.img
-
-./platform-tools/fastboot flash modem_a ./images/modem.img
-
-./platform-tools/fastboot flash multiimgoem_a ./images/multiimgoem.img
-
-./platform-tools/fastboot flash odm_a ./images/odm.img
-
-./platform-tools/fastboot flash product_a ./images/product.img
-
-./platform-tools/fastboot flash qupfw_a ./images/qupfw.img
-
-./platform-tools/fastboot flash shrm_a ./images/shrm.img
-
-./platform-tools/fastboot flash system_a ./images/system.img
-
-./platform-tools/fastboot flash system_ext_a ./images/system_ext.img
-
-./platform-tools/fastboot flash tz_a ./images/tz.img
-
-./platform-tools/fastboot flash uefisecapp_a ./images/uefisecapp.img
-
-./platform-tools/fastboot flash vbmeta_a ./images/vbmeta.img
-
-./platform-tools/fastboot flash vbmeta_system_a ./images/vbmeta_system.img
-
-./platform-tools/fastboot flash vendor_a ./images/vendor.img
-
-./platform-tools/fastboot flash vendor_boot_a ./images/vendor_boot.img
-
-./platform-tools/fastboot flash xbl_a ./images/xbl.img
-
-./platform-tools/fastboot flash xbl_config_a ./images/xbl_config.img
-
+./platform-tools/fastboot flash boot_$slot ./images/boot.img
+./platform-tools/fastboot flash cpucp_$slot ./images/cpucp.img
+./platform-tools/fastboot flash devcfg_$slot ./images/devcfg.img
+./platform-tools/fastboot flash dsp_$slot ./images/dsp.img
+./platform-tools/fastboot flash dtbo_$slot ./images/dtbo.img
+./platform-tools/fastboot flash featenabler_$slot ./images/featenabler.img
+./platform-tools/fastboot flash hyp_$slot ./images/hyp.img
+./platform-tools/fastboot flash imagefv_$slot ./images/imagefv.img
+./platform-tools/fastboot flash keymaster_$slot ./images/keymaster.img
+./platform-tools/fastboot flash modem_$slot ./images/modem.img
+./platform-tools/fastboot flash multiimgoem_$slot ./images/multiimgoem.img
+./platform-tools/fastboot flash odm_$slot ./images/odm.img
+./platform-tools/fastboot flash product_$slot ./images/product.img
+./platform-tools/fastboot flash qupfw_$slot ./images/qupfw.img
+./platform-tools/fastboot flash shrm_$slot ./images/shrm.img
+./platform-tools/fastboot flash system_$slot ./images/system.img
+./platform-tools/fastboot flash system_ext_$slot ./images/system_ext.img
+./platform-tools/fastboot flash tz_$slot ./images/tz.img
+./platform-tools/fastboot flash uefisecapp_$slot ./images/uefisecapp.img
+./platform-tools/fastboot flash vbmeta_$slot ./images/vbmeta.img
+./platform-tools/fastboot flash vbmeta_system_$slot ./images/vbmeta_system.img
+./platform-tools/fastboot flash vendor_$slot ./images/vendor.img
+./platform-tools/fastboot flash vendor_boot_$slot ./images/vendor_boot.img
+./platform-tools/fastboot flash xbl_$slot ./images/xbl.img
+./platform-tools/fastboot flash xbl_config_$slot ./images/xbl_config.img
 ./platform-tools/fastboot reboot bootloader
 
-fastboot --set-active=b
-
-echo "Flashing B slot. Please wait..."
-
-fastboot reboot fastboot
-
-fastboot flash abl_b ./images/abl.img
-
-fastboot flash aop_b ./images/aop.img
-
-fastboot flash bluetooth_b ./images/bluetooth.img
-
-fastboot flash boot_b ./images/boot.img
-
-fastboot flash cpucp_b ./images/cpucp.img
-
-fastboot flash devcfg_b ./images/devcfg.img
-
-fastboot flash dsp_b ./images/dsp.img
-
-fastboot flash dtbo_b ./images/dtbo.img
-
-fastboot flash featenabler_b ./images/featenabler.img
-
-fastboot flash hyp_b ./images/hyp.img
-
-fastboot flash imagefv_b ./images/imagefv.img
-
-fastboot flash keymaster_b ./images/keymaster.img
-
-fastboot flash modem_b ./images/modem.img
-
-fastboot flash multiimgoem_b ./images/multiimgoem.img
-
-fastboot flash odm_b ./images/odm.img
-
-fastboot flash product_b ./images/product.img
-
-fastboot flash qupfw_b ./images/qupfw.img
-
-fastboot flash shrm_b ./images/shrm.img
-
-fastboot flash system_b ./images/system.img
-
-fastboot flash system_ext_b ./images/system_ext.img
-
-fastboot flash tz_b ./images/tz.img
-
-fastboot flash uefisecapp_b ./images/uefisecapp.img
-
-fastboot flash vbmeta_b ./images/vbmeta.img
-
-fastboot flash vbmeta_system_b ./images/vbmeta_system.img
-
-fastboot flash vendor_b ./images/vendor.img
-
-fastboot flash vendor_boot_b ./images/vendor_boot.img
-
-fastboot flash xbl_b ./images/xbl.img
-
-fastboot flash xbl_config_b ./images/xbl_config.img
-
-fastboot reboot bootloader
-
-fastboot --set-active=a
-
-fastboot $* reboot
-echo ""
-echo "Your phone(1) is rebooting"
-
+echo "${GREEN}${BOLD}Your phone(1) is rebooting"
 read -p "Press enter to continue"
