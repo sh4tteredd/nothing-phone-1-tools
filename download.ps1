067c45ef1a8acd77655e0e4e0164d29edff303d3
sh4tteredd <55893559+sh4tteredd@users.noreply.github.com>
2023-08-02 13:06:38 +0200

    1.5.5 update

--- download.ps1
+++ download.ps1
@@ -19,7 +19,7 @@ function clean {
     
 }
 #Test-CommandExists("fastboot")
-Write-Host "Nothing firmware downloader by @sh4ttered V1.1.7"
+Write-Host "Nothing firmware downloader by @sh4ttered V1.5.5"
 $msg1 = 'Have you already downloaded the firmware? (y/n)'
 
 $choice = Read-Host -Prompt $msg1
@@ -29,24 +29,11 @@ if ($choice -eq 'y') {
     $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
 else {
-    $msg = 'Do you need the [G]lobal firmware or the [E]uropean firmware (G/E)? '
-    $response = Read-Host -Prompt $msg
-    if ($response -eq 'g') {
-        Write-Host "Downloading the global firmware v1.1.7"
-        Write-Host "This may take a while depending on your internet speed"
-        wget -Uri https://android.googleapis.com/packages/ota-api/package/254815bb72cdbddd5c9dd7cde6d10c95becc6542.zip -OutFile fw.zip #global 1.1.7
-    }
-    elseif ($response -eq 'e') {
-        Write-Host "Downloading the EU firmware v1.1.7"
-        Write-Host "This may take a while depending on your internet speed"
-        wget -Uri https://android.googleapis.com/packages/ota-api/package/0e6855d19dbcdf328449e4d06386a6257bb1aadd.zip -OutFile fw.zip #eu 1.1.7
-    }
-    else {
-        Write-Host "Invalid Input!"
-        Write-Host -NoNewLine 'Press any key to continue...';
-        $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-        exit
-    }
+    
+    Write-Host "Downloading the V.1.5.5 firmware"
+    Write-Host "This may take a while depending on your internet speed"
+    wget -Uri https://android.googleapis.com/packages/ota-api/package/1d156af4eb59f85c62c7921e6c4a97c2761bcc3b.zip -OutFile fw.zip #global/EEA 1.5.5
+    
 }
 Add-Type -Assembly System.IO.Compression.FileSystem
 $zip = [IO.Compression.ZipFile]::OpenRead(".\fw.zip")
--- download.sh
+++ download.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 check() {
-      if ! command -v $1 > /dev/null; then
-      echo "$1 is not installed!"
-      echo "In order to use this script please install $1 from your package manager"
-      exit
-  fi
+    if ! command -v $1 > /dev/null; then
+        echo "$1 is not installed!"
+        echo "In order to use this script please install $1 from your package manager"
+        exit
+    fi
 }
 
 clean() {
@@ -17,24 +17,14 @@ clean() {
 }
 
 download(){
-
-read -p "Do you need the [G]lobal firmware or the [E]uropean firmware (G/E)? " ge 
-case $ge in 
-	G | g)  echo "Downloading the global firmware v1.1.7";
-            echo "This may take a while depending on your internet speed";
-            echo " ";
-            wget -q --show-progress -O fw.zip https://android.googleapis.com/packages/ota-api/package/254815bb72cdbddd5c9dd7cde6d10c95becc6542.zip;;
-	E | e)  echo "Downloading the EU firmware v1.1.7";
-            echo "This may take a while depending on your internet speed";
-            echo " ";
-            wget -q --show-progress -O fw.zip https://android.googleapis.com/packages/ota-api/package/0e6855d19dbcdf328449e4d06386a6257bb1aadd.zip;;
-    * )     echo "Invalid input!";
-		    exit 1;;
-esac
-
-
     
+    read -p "Do you need the [G]lobal firmware or the [E]uropean firmware (G/E)? " ge
+    echo "Downloading the V.1.5.5 firmware";
+    echo "This may take a while depending on your internet speed";
+    echo " ";
+    wget -q --show-progress -O fw.zip https://android.googleapis.com/packages/ota-api/package/1d156af4eb59f85c62c7921e6c4a97c2761bcc3b.zip;;
 }
+
 echo "Nothing firmware downloader by @sh4ttered V1.1.7"
 check wget
 check tar
@@ -42,11 +32,11 @@ check unzip
 rm -rf images/ #clean from previous downloads
 
 if [[ $(uname -m) == 'arm64' ]]; then #check if arch is arm64
-  arm="1"
+    arm="1"
 fi
 if [[ $(uname) == 'Linux' ]]; then #GNU/Linux
     linux="1"
-elif [[ $(uname) == 'Darwin' ]]; then #macOS
+    elif [[ $(uname) == 'Darwin' ]]; then #macOS
     mac="1"
 else
     echo "OS Unsupported"
@@ -55,11 +45,11 @@ fi
 
 read -p "Have you already downloaded the firmware? (y/n)" choice
 case "$choice" in
-  y|Y ) echo "Skipping download";;
-  n|N ) echo "Downloading firmware..."
-        download;;
+    y|Y ) echo "Skipping download";;
+    n|N ) echo "Downloading firmware..."
+    download;;
     * ) echo "Invalid input!";
-        exit 1;;
+    exit 1;;
 esac
 
 if [[ $linux -eq "1" ]]; then #GNU/Linux
@@ -68,7 +58,7 @@ if [[ $linux -eq "1" ]]; then #GNU/Linux
     else
         wget -q -O payload-dumper-go.tar.gz https://github.com/ssut/payload-dumper-go/releases/download/1.2.2/payload-dumper-go_1.2.2_linux_amd64.tar.gz
     fi
-elif [[ $mac -eq "1" ]]; then #macOS
+    elif [[ $mac -eq "1" ]]; then #macOS
     if [[ $arm -eq 1 ]]; then #download arm64 version if needed
         wget -q -O payload-dumper-go.tar.gz https://github.com/ssut/payload-dumper-go/releases/download/1.2.2/payload-dumper-go_1.2.2_darwin_arm64.tar.gz
     else
@@ -88,8 +78,8 @@ echo " "
 mkdir images
 mv extracted*/* images/
 clean;
-read -p "Do you want to flash your phone now? (y/N)? " yn 
-if [[ $yn -eq y ]]; then 
+read -p "Do you want to flash your phone now? (y/N)? " yn
+if [[ $yn -eq y ]]; then
     ./flash_all.sh
 else
     exit 1
